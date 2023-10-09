326b96d79dbee61a266914c6d097e3ade5a8ebc6
ICE0208 <doky3307@naver.com>
2023-10-10 04:46:56 +0900

    (#1.3) Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDMKtNHKuZ4v5kcPbPxnWBSYRkD-LNMFmY",
+  authDomain: "nwitter-reloaded-beb9d.firebaseapp.com",
+  projectId: "nwitter-reloaded-beb9d",
+  storageBucket: "nwitter-reloaded-beb9d.appspot.com",
+  messagingSenderId: "833974907765",
+  appId: "1:833974907765:web:6454331542317818c3193c",
+};
+
+const app = initializeApp(firebaseConfig);
